a4ed618c7b401a6bb28a23f804a0a2c426d6ba87
kakazinh1 <blackspidermanrdj@gmail.com>
2025-05-29 00:00:52 +0000

    resultado final

--- main.py
+++ main.py
@@ -7,10 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/base')
+@app.route('/tokyo')
 
 def primeira():
-    return render_template('base.html')
+    return render_template('tokyo.html')
+
+@app.route('/kyoto')
+
+def segunda():
+    return render_template('kyoto.html')
+
+@app.route('/osaka')
+
+def terceira():
+    return render_template('osaka.html')
+
+@app.route('/okinawa')
+
+def quarta():
+    return render_template('okinawa.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
